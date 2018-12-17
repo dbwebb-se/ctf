6b0efb0892296616bea1153889e3b0f2c00e3083
Kenneth Lewenhagen <lewenhagen@live.com>
2018-12-17 14:29:23 +0100

    restructured solve.bash

--- src/0003/script/solve.bash
+++ src/0003/script/solve.bash
@@ -2,14 +2,22 @@
 #
 # Solve the dbwebb ctf and echo out the flag.
 #
+targetFile="7cd86ecb09aa48c6e620b340f6a74592.zip"
+endpoint="target/movies/Action/Die Hard/Die Hard 3/flag.txt"
+
+# Create the working directory and download the target file
+root="$( pwd )"
+workDir="$root/build/solve"
+rm -rf "$workDir"
+install -d "$workDir"
+cd "$workDir" || exit
+cp "$root/target/$targetFile" "$targetFile"
 
 # Get the flag from the target
-install -d build/solve
-cd build/solve || exit
+mv "$targetFile" file.zip
+unzip -oq file.zip
 
-cp "$( find ../../target -mindepth 1 )" file.zip
-unzip file.zip
-cat flag.txt
+cat "$endpoint"
 
-# Verify it matches the content of flag.txt
-diff flag.txt ../../flag.txt
+# Verify the flag matches the content of flag.txt
+diff "$root/flag.txt" "$endpoint"
